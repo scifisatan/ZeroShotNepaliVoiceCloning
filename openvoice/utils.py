afb9fdf6487b327b09b0cb8429d3199a750f6f3e
scifisatan <brawlershere@gmail.com>
2025-01-26 11:13:52 +0545

    Remove unused functions and clean up code in commons, mel_processing, and utils modules

--- openvoice/utils.py
+++ openvoice/utils.py
@@ -43,38 +43,6 @@ class HParams:
         return self.__dict__.__repr__()
 
 
-def string_to_bits(string, pad_len=8):
-    # Convert each character to its ASCII value
-    ascii_values = [ord(char) for char in string]
-    
-    # Convert ASCII values to binary representation
-    binary_values = [bin(value)[2:].zfill(8) for value in ascii_values]
-    
-    # Convert binary strings to integer arrays
-    bit_arrays = [[int(bit) for bit in binary] for binary in binary_values]
-    
-    # Convert list of arrays to NumPy array
-    numpy_array = np.array(bit_arrays)
-    numpy_array_full = np.zeros((pad_len, 8), dtype=numpy_array.dtype)
-    numpy_array_full[:, 2] = 1
-    max_len = min(pad_len, len(numpy_array))
-    numpy_array_full[:max_len] = numpy_array[:max_len]
-    return numpy_array_full
-
-
-def bits_to_string(bits_array):
-    # Convert each row of the array to a binary string
-    binary_values = [''.join(str(bit) for bit in row) for row in bits_array]
-    
-    # Convert binary strings to ASCII values
-    ascii_values = [int(binary, 2) for binary in binary_values]
-    
-    # Convert ASCII values to characters
-    output_string = ''.join(chr(value) for value in ascii_values)
-    
-    return output_string
-
-
 def split_sentence(text, min_len=10, language_str='[EN]'):
     if language_str in ['EN']:
         sentences = split_sentences_latin(text, min_len=min_len)
